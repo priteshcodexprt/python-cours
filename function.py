66da45cbaa3f58336c627633f8e87ee2ce83c314
“pritesh” <pritesvadoliya5@gmail.com>
2022-06-09 10:40:18 +0400

    python

---
+++ function.py
@@ -0,0 +1,6 @@
+marks=[45,85,42,78]
+percentage1=(sum(marks)/400)*100
+
+marks=[25,95,45,76]
+percentage2=(sum(marks)/400)*100
+print(percentage1,percentage1)
---
+++ quick_quiz.py
@@ -0,0 +1,9 @@
+def greet(name):
+    print("Good day, "+name)
+
+def mySum(num1,num2):
+    return num1 + num2
+
+greet("Pritesh")
+s =mySum(6,32)
+print(s)
