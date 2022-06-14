d67bb9ce77358c7c1bcaa98ab6590eb19df5a85f
“pritesh” <pritesvadoliya5@gmail.com>
2022-06-14 15:25:56 +0400

    master

--- prectic/input.py
+++ prectic/input.py
@@ -2,9 +2,25 @@ post=input("Enter your post \n")
 
 postx=post.capitalize()
 
-if postx in "Sagar":
+if postx in "Sagar," :
 
-    print("Present")
+
+        print("Present")
+
+else:
+
+    print("Not Present")
+
+
+
+post=input("Enter your post \n")
+
+postx=post.capitalize()
+
+if postx in "pritesh" :
+
+
+        print("Present")
 
 else:
 
