a16a858148bd0e595821d500e13938d403fe1957
“pritesh” <pritesvadoliya5@gmail.com>
2022-06-17 12:41:06 +0400

    master

--- oops/concsepct_method_.py
+++ oops/concsepct_method_.py
@@ -2,16 +2,11 @@ class Employee:
     company="Goggle"
 
 
-    def __init__(self,name,salary,subunit):
-        print("Employee is created!")
+    def ___init___ (self,name,salary,subunit):
+        self.name=name
+        self.salary=salary
+        self.subunit=subunit
+        print("Employee is created")
 
-    def getSalary(self,signature):
-        print(f"salary for this employee working in {self.company} is: {self.salary}\n{signature}")
-    @staticmethod
-    def greet():
-        print("Good morning,sir ")
-    @staticmethod
-    def time ():
-        print("The time is 9AM in the morning")
-
-harry= Employee("Harry",100,"Youtube")
+    def getDetails(self):
+        print("The name of the employee is ")
